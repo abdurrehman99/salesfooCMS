8bd4f250c9cc362ebc585a4de28409e443ab9a56
abdurrehman99 <syed.ebad1999@gmail.com>
2020-10-25 14:53:09 +0500

    production styling issue fix

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import useStyles from "./style";
+import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -21,6 +21,63 @@ import MenuOutlinedIcon from "@material-ui/icons/MenuOutlined";
 import NotificationsOutlinedIcon from "@material-ui/icons/NotificationsOutlined";
 import MoreVertOutlinedIcon from "@material-ui/icons/MoreVertOutlined";
 import Avatar from "@material-ui/core/Avatar";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    background: "#f0f2f5",
+  },
+  drawer: {
+    width: 104,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 104,
+    background: theme.palette.primary.main,
+  },
+  drawerContainer: {
+    overflow: "auto",
+    color: "white",
+    marginTop: 45,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+  },
+  toolbar: {
+    paddingLeft: 10,
+    paddingRight: 45,
+    height: 100,
+  },
+  whiteIcon: {
+    color: "white",
+    margin: "auto",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  blackIcon: {
+    color: "black",
+    cursor: "pointer",
+    marginRight: 15,
+  },
+  menuIcon: {
+    color: "black",
+    cursor: "pointer",
+    marginRight: 20,
+    marginLeft: 30,
+  },
+  navLogo: {
+    width: 218,
+    height: 65,
+    marginRight: "auto",
+  },
+  avatar: {
+    cursor: "pointer",
+  },
+}));
 
 const Index = ({ children }) => {
   const classes = useStyles();
